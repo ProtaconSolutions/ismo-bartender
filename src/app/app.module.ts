34419d455b55f6d9bbf34e75e4a7592580450ea1
sah <sami.ahonen@protacon.com>
2016-12-15 12:39:20 +0200

    Added ismo module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { IsmoComponent } from './modules/ismo/ismo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IsmoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/ismo/ismo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ismo',
+  templateUrl: './ismo.component.html',
+  styleUrls: ['./ismo.component.css']
+})
+export class IsmoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
