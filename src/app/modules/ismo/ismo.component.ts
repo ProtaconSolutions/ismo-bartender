1dd70ac945657c0935478108f64790c67a5197c5
dopson <vili.tielinen@outlook.com>
2016-12-15 13:28:37 +0200

    Added a button for sending a test order

--- src/app/modules/ismo/ismo.component.ts
+++ src/app/modules/ismo/ismo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from "../../shared/services/order.service";
+import {Order} from "../../shared/services/models/order";
 
 @Component({
   selector: 'app-ismo',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IsmoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
   }
 
+  public sendTestOrder() : void {
+    var testOrder = new Order();
+    testOrder.state = 123;
+    testOrder.recipe = [ 0, 0, 1, 0, 0, 3 ];
+    this.orderService.sendOrder(testOrder);
+  }
 }
--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -3,13 +3,12 @@ import { AngularFire } from 'angularfire2';
 import { Order } from './models/order';
 
 @Injectable()
-
 export class OrderService {
 
   constructor(private angularFire: AngularFire) {
   }
 
-  sendOrder(order : Order) {
+  sendOrder(order: Order) {
     let orders = this.angularFire.database.list('orders');
 
     orders.push(order);
